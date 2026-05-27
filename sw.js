99550ce20f1a47e71b4e0f80bc7f3f67faa425fc
MARUF <145954185+ixmaruf@users.noreply.github.com>
2026-05-27 17:48:32 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
      3) Product images NEVER re-downloaded once cached
    ════════════════════════════════════════════════════════════════════ */
 
-const VERSION       = 'yarz-turbo-v15.64-2025-05-27';
+const VERSION       = 'yarz-turbo-v15.66-2025-05-27';
 const STATIC_CACHE  = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const IMAGE_CACHE   = `${VERSION}-images`;
